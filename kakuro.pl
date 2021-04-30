5065ca7be01fb9718cb1d41fd6eaa1b8053e6035
Rafael Oliveira <rafdev.x@gmail.com>
2021-04-30 15:18:01 +0100

    more espaco_fila

--- kakuro.pl
+++ kakuro.pl
@@ -23,9 +23,34 @@ sem_listas([]).
 sem_listas([E]) :- \+(is_list(E)), !.
 sem_listas([P | R]) :- \+(is_list(P)), sem_listas(R).
 
-obter_soma(H, V, Dir, Soma) :- Dir = h -> Soma = H; Soma = V.
+soma_dir([H, V], Dir, Soma) :- Dir = h -> Soma = H; Soma = V.
 
-espaco_fila([[H | V] | R], Esp, Dir) :-
+separar_ultimo([E], [], E).
+separar_ultimo([P, Q], [P], Q).
+separar_ultimo([P | R], I, Ultimo) :-
+	separar_ultimo(R, IParcial, Ultimo),
+	append([P], IParcial, I).
+
+/*espaco_fila([[H, V] | R], Esp, Dir) :-
 	sem_listas(R),
-	obter_soma(H, V, Dir, Soma),
-	Esp = espaco(Soma, R).
+	soma_dir([H, V], Dir, Soma),
+	Esp = espaco(Soma, R).*/
+
+espaco_fila(Fila, Esp, Dir) :-
+	espaco_fila_aux(Fila, Esps, Dir, []),
+	member(Esp, Esps).
+
+espaco_fila_aux([], _, _, _). % (*)
+espaco_fila_aux(Fila, Esps, Dir, Acc) :-
+	separar_ultimo(Fila, R, Ultimo),
+	is_list(Ultimo), !,
+	soma_dir(Ultimo, Dir, Soma),
+	Esp = espaco(Soma, Acc),
+	append([Esp], Esps, NEsps),
+	espaco_fila_aux(R, NEsps, Dir, []).
+espaco_fila_aux(Fila, Esps, Dir, Acc) :-
+	separar_ultimo(Fila, R, Ultimo),
+	append([Ultimo], Acc, NAcc),
+	espaco_fila_aux(R, Esps, Dir, NAcc).
+
+/* falta casas vazias */
