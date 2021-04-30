9dfa272143459e8605dd58525acfdff17d5628c8
Rafael Oliveira <rafdev.x@gmail.com>
2021-04-30 21:34:00 +0100

    fix bugs

--- kakuro.pl
+++ kakuro.pl
@@ -27,26 +27,29 @@ separar_ultimo([P | R], I, Ultimo) :-
 	separar_ultimo(R, IParcial, Ultimo),
 	append([P], IParcial, I).
 
-espaco_nao_vazio(espaco(Soma, _)) :- Soma > 0.
+espaco_vazio(espaco(_, Els)) :- length(Els, 0).
 
-espacos_fila([], Esps, _, [], AccEsps) :-
+espacos_fila_aux([], Esps, _, [], AccEsps) :-
 	!,
-	include(espaco_nao_vazio, AccEsps, Esps). % (*)
-espacos_fila(Fila, Esps, Dir, AccEls, AccEsps) :-
+	exclude(espaco_vazio, AccEsps, Esps). % (*)
+espacos_fila_aux(Fila, Esps, Dir, AccEls, AccEsps) :-
 	separar_ultimo(Fila, R, Ultimo),
 	is_list(Ultimo),
 	!,
 	soma_dir(Ultimo, Dir, Soma),
 	Esp = espaco(Soma, AccEls),
 	append([Esp], AccEsps, NEsps),
-	espacos_fila(R, Esps, Dir, [], NEsps).
-espacos_fila(Fila, Esps, Dir, AccEls, AccEsps) :-
+	espacos_fila_aux(R, Esps, Dir, [], NEsps).
+espacos_fila_aux(Fila, Esps, Dir, AccEls, AccEsps) :-
 	separar_ultimo(Fila, R, Ultimo),
 	\+(is_list(Ultimo)),
 	!,
 	append([Ultimo], AccEls, NEls),
-	espacos_fila(R, Esps, Dir, NEls, AccEsps).
+	espacos_fila_aux(R, Esps, Dir, NEls, AccEsps).
+
+espacos_fila(Dir, Fila, Esps) :-
+	espacos_fila_aux(Fila, Esps, Dir, [], []).
 
 espaco_fila(Fila, Esp, Dir) :-
-	espacos_fila(Fila, Esps, Dir, [], []),
+	espacos_fila(Dir, Fila, Esps),
 	member(Esp, Esps).
