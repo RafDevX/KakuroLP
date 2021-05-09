8dfe383f3d4ce6be87b7f49ae86e24b9c71726d2
Rafael Oliveira <rafdev.x@gmail.com>
2021-05-09 21:58:23 +0100

    findall -> setof

--- kakuro.pl
+++ kakuro.pl
@@ -115,9 +115,9 @@ permutacao_possivel_espaco(Perm, Esp, Espacos, Perms_soma) :-
 	espacos_com_posicoes_comuns(Espacos, Esp, Comuns),
 	maplist(permutacao_possivel_espaco_aux(Perms_soma, Esp, Perm), Comuns).
 
-permutacoes_possiveis_espaco(Espacos, Perms_soma, Esp, [Els, Poss]) :-
+permutacoes_possiveis_espaco(Espacos, P_soma, Esp, [Els, Poss]) :-
 	Esp = espaco(_, Els),
-	findall(P, permutacao_possivel_espaco(P, Esp, Espacos, Perms_soma), Poss).
+	setof(P, P_soma^permutacao_possivel_espaco(P, Esp, Espacos, P_soma), Poss).
 
 permutacoes_possiveis_espacos(Esps, Perms_poss_esps) :-
 	permutacoes_soma_espacos(Esps, Ps_soma),
